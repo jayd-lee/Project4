6dea61b1422efef197dd18f6f58c4b91001668d9
Jayden Lee <jaydeh.lee@gmail.com>
2023-04-05 17:07:51 -0400

    First Commit aadi by jayden

--- src/project4.java
+++ src/project4.java
@@ -1,5 +1,9 @@
 public class project4 {
     public static void main(String[] args) {
         System.out.println("Aadifad");
+
+        System.out.println("Jayden Branch");
+        System.out.println("Aadi Branch");
+
     }
 }
