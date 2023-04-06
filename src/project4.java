4bc56c2484714b412a18a4af7b3c1016db5fcaca
Jayden Lee <113277293+jayd-lee@users.noreply.github.com>
2023-04-06 12:55:27 -0400

    Update project4.java

--- src/project4.java
+++ src/project4.java
@@ -2,5 +2,6 @@ public class project4 {
     public static void main(String[] args) {
         System.out.println("dhansuh");
         System.out.printf("daighdiwopghawioeghewaio");
+        System.out.printf("daighdiwopghawioeghewaio");
     }
 }
