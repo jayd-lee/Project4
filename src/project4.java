846393d27dce99fc2d176b2c2e5bc30cf9ebac45
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-06 12:50:42 -0400

    Dhanush first commit

--- src/project4.java
+++ src/project4.java
@@ -1,6 +1,6 @@
 public class project4 {
     public static void main(String[] args) {
         System.out.println("dhansuh");
-        System.out.printf("daighdiwopghawioeghewaio");
+        System.out.printf("");
     }
 }
