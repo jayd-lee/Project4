84a0fa94881aad9cfda8ae354ae15a2755c1313b
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-09 16:00:25 -0400

    made daddy user

--- src/Customer.java
+++ src/Customer.java
@@ -1,30 +1,7 @@
 import java.util.ArrayList;
 
-public class Customer {
-    private String name;
-    private String password;
-    private String email;
+public class Customer extends User {
     public Customer(String name, String password, String email) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setEmail(String email) {
-        this.email = email;
+        super(name, password, email);
     }
 }
