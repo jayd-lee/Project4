30a72e30765937c5a6029c81c19b2296db805200
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-09 17:53:31 -0400

    Dhanush

--- src/Customer.java
+++ src/Customer.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 
 public class Customer extends User {
+    private String messageFileName;
+    private ArrayList<String> blockedSellers;
     public Customer(String name, String password, String email) {
         super(name, password, email);
+        this.blockedSellers = new ArrayList<String>();
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,7 +1,21 @@
 import java.util.ArrayList;
 
-public class Seller extends User{
-    public Seller(String name, String password, String email) {
-        super( name,  password,  email);
+public class Seller extends User {
+    private String messageFileName;
+    private ArrayList<String> stores;
+    private ArrayList<String> blockedCustomers;
+    public Seller(String name, String password, String email, String messageFileName) {
+        super(name, password, email);
+        this.messageFileName = messageFileName;
+        this.stores = new ArrayList<String>();
+        this.blockedCustomers = new ArrayList<String>();
+    }
+    public void addStore(String store) {
+        this.stores.add(store);
+    }
+    public void removeStore(String store) {
+        this.stores.remove(store);
     }
 }
+
+
