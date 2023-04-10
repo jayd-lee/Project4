1868671f7cbf03d741cfa4e943b00aee601dd741
DhanushManjunath <129692872+DhanushManjunath@users.noreply.github.com>
2023-04-10 17:21:00 -0400

    Update Seller.java

--- src/Seller.java
+++ src/Seller.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 import java.io.*;
-
+/**
+ * Seller.java
+ *
+ * Creates an ArrayList of all the sellers that are on the database
+ *
+ * @author Dhanush Manjunath, Aadi Gupta, Jayden Lee, Kylie Houston, LE2
+ *
+ * @version 4/10/23
+ *
+ */
 public class Seller {
     private ArrayList<String> sellers = new ArrayList<String>();
     public Seller() {
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File("src/seller.txt")));
             String line = br.readLine();
-            while(line != null) {
+            while (line != null) {
                 String[] details = line.split(",");
                 String name = details[0].trim();
                 sellers.add(name);
                 line = br.readLine();
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -21,5 +30,4 @@ public class Seller {
         return sellers;
     }
 }
-
 
