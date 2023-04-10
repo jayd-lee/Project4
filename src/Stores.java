20795408496ab69fd68d8d7349af2d02243949dd
DhanushManjunath <129692872+DhanushManjunath@users.noreply.github.com>
2023-04-10 17:21:28 -0400

    Update Stores.java

--- src/Stores.java
+++ src/Stores.java
@@ -1,17 +1,26 @@
 import java.io.*;
 import java.util.ArrayList;
-
+/**
+ * Stores.java
+ *
+ * Creates an ArrayList of all of the stores that are on the database
+ *
+ * @author Dhanush Manjunath, Aadi Gupta, Jayden Lee, Kylie Houston, LE2
+ *
+ * @version 4/10/23
+ *
+ */
 public class Stores {
     private ArrayList<String> stores = new ArrayList<String>();
-    public Stores(){
+    public Stores() {
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File("src/stores.txt")));
             String line = br.readLine();
-            while(line != null) {
+            while (line != null) {
                 stores.add(line);
                 line = br.readLine();
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -19,12 +28,12 @@ public class Stores {
         return stores;
     }
     public void writeStores(ArrayList<String> storesNew) {
-        try{
+        try {
             FileWriter fw = new FileWriter("src/stores.txt");
             PrintWriter pw = new PrintWriter(fw);
             int size = (storesNew != null) ? storesNew.size() : 0;
 
-            for(String store : storesNew) {
+            for (String store : storesNew) {
                 pw.println(store);
             }
 
