ba04f6a0115f51980bd5beb0d56c162dcd6853dd
DhanushManjunath <129692872+DhanushManjunath@users.noreply.github.com>
2023-04-10 17:20:36 -0400

    Update Messages.java

--- src/Messages.java
+++ src/Messages.java
@@ -1,17 +1,26 @@
 import java.io.*;
 import java.util.ArrayList;
-
+/**
+ * Messages.java
+ *
+ * Creates an ArrayList of all of the messages that are on the database
+ *
+ * @author Dhanush Manjunath, Aadi Gupta, Jayden Lee, Kylie Houston, LE2
+ *
+ * @version 4/10/23
+ *
+ */
 public class Messages {
     private ArrayList<String> messages = new ArrayList<String>();
     public Messages() {
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File("src/messages.txt")));
             String line = br.readLine();
-            while(line != null) {
+            while (line != null) {
                 messages.add(line);
                 line = br.readLine();
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -19,11 +28,11 @@ public class Messages {
         return messages;
     }
     public void writeMessages(ArrayList<String> messagesNew) {
-        try{
+        try {
             FileWriter fw = new FileWriter("src/messages.txt");
             PrintWriter pw = new PrintWriter(fw);
 
-            for(String message : messagesNew) {
+            for (String message : messagesNew) {
                 pw.println(message);
             }
             pw.close();
