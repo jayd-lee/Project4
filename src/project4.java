5fb83c9776f42d33bb72a4f90845fac4901d069b
Dhanush Manjunath <dmanjun@purdue.edu>
2023-04-09 20:17:59 -0400

    project4 and messages

--- src/project4.java
+++ src/project4.java
@@ -1,10 +1,79 @@
 import java.util.Scanner;
 import java.io.*;
 import java.util.TreeMap;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class project4 {
     static Scanner scan = new Scanner(System.in);
+    private Messages message;
+    private ArrayList<String> messages;
+    private ArrayList<String> stores;
+    private ArrayList<String> sellers;
+    private ArrayList<String> customers;
+    public project4() {
+        message = new Messages();
+        messages = message.getMessages();
+    }
+    public void setStores() {
+
+    }
+    public void setSellers() {
+
+    }
+    public void setCustomers() {
+        
+    }
+    public void createMessage(String sender, String receiver, String message) {
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String line = sender + "," + receiver + "," + sdf.format(ts) + "," + message + "," + "false";
+        messages.add(line);
+    }
+    public void updateMessage(String sender, String receiver, String message, String newMessage) {
+        String[] line;
+        int index = -1;
+        for(int x = 0; x < messages.size(); x++) {
+            line = messages.get(x).split(",");
+            if (line[0].equalsIgnoreCase(sender) && line[1].equalsIgnoreCase(receiver) && line[3].equalsIgnoreCase(message)) {
+                index = x;
+            }
+        }
+        messages.remove(index);
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String data = sender + "," + receiver + "," + sdf.format(ts) + "," + newMessage + "," + "false";
+        messages.add(data);
+    }
+    public void deleteMessage(String sender, String receiver, String message, String newMessage) {
+        String[] line;
+        int index = -1;
+        String send = "";
+        String receive = "";
+        String time = "";
+        String mess = "";
+        for(int x = 0; x < messages.size(); x++) {
+            line = messages.get(x).split(",");
+            if (line[0].equalsIgnoreCase(sender) && line[1].equalsIgnoreCase(receiver) && line[3].equalsIgnoreCase(message)) {
+                index = x;
+                send = line[0];
+                receive = line[1];
+                time = line[2];
+                mess = line[3];
+            }
+        }
+        messages.remove(index);
+        String data = send + "," + receive + "," + time + "," + mess + "," + "true";
+        messages.add(data);
+    }
+    public void write() {
+        message.writeMessages(messages);
+    }
     public static void main(String[] args) throws IOException {
+        Messages message = new Messages();
+        boolean custom;
         System.out.println("Welcome to the chat app!");
         System.out.println("1. Login");
         System.out.println("2. Sign up");
@@ -16,14 +85,13 @@ public class project4 {
                 System.out.println("1. Seller Login");
                 System.out.println("2. Customer Login");
                 System.out.println("3. Exit");
-
                 do {
                     choice = scan.nextLine();
                 } while (!choice.equals("3") && !choice.equals("2") && !choice.equals("1"));
 
                 login(choice);
+                custom = false;
                 break;
-
             case "2":
                 System.out.println("1. Seller SignUp");
                 System.out.println("2. Customer Signup");
@@ -33,19 +101,27 @@ public class project4 {
                 } while (!choice.equals("3") && !choice.equals("2") && !choice.equals("1"));
 
                 signup(choice);
-
+                custom = true;
                 break;
 
             case "3":
                 System.out.println("Thanks for using the chat app!");
                 return;
-
             default:
                 System.out.println("Invalid choice");
                 main(args);
                 break;
+        
         }
-
+        if (custom) {
+            System.out.println("1. Browse clothing stores.");
+            System.out.println("2. Enter the seller's name who you want to contact.");
+            int input = scan.nextInt();
+            scan.nextLine();
+            if (input == 1) {
+                
+            }
+        }
     }
 
     public static void login(String choice) throws IOException {
@@ -59,9 +135,9 @@ public class project4 {
             boolean usernameExists = false;
             BufferedReader br = null;
             try {
-                br = new BufferedReader(new FileReader("src/seller.txt"));
-                String line;
-                while (loginUsername == null) {
+                while(!usernameExists) {
+                    br = new BufferedReader(new FileReader("seller.txt"));
+                    String line;
                     while ((line = br.readLine()) != null) {
                         if (line.toLowerCase().contains(username.toLowerCase())) {
                             usernameExists = true;
@@ -73,8 +149,8 @@ public class project4 {
                         }
                     }
                     System.out.println("Username does not exist");
-                }
-
+                    username = Tools.username();
+            }
             } catch (IOException e) {
                 System.out.println("Username does not exist");
                 e.printStackTrace();
